c6c8d6c2e6364d375143c23cc5db847e82cef8ab
고나은 <gonaeunn@gmail.com>
2025-04-03 14:41:04 +0900

    spring boot 환경설정

---
+++ src/main/java/com/example/schedulerprojectjpa/SchedulerProjectJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulerprojectjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerProjectJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerProjectJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulerprojectjpa/SchedulerProjectJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulerprojectjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerProjectJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
