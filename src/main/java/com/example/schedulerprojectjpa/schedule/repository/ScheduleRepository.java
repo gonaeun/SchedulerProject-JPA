371f21bc1af489bb31f14c4f58f5d1dedbadf9e5
고나은 <gonaeunn@gmail.com>
2025-04-06 20:30:02 +0900

    refactor: jpa방식으로 repository 수정

--- src/main/java/com/example/schedulerprojectjpa/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/example/schedulerprojectjpa/schedule/repository/ScheduleRepository.java
@@ -1,19 +1,8 @@
 package com.example.schedulerprojectjpa.schedule.repository;
 
 import com.example.schedulerprojectjpa.schedule.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface ScheduleRepository {
-    // 이거 인터페이스니까 >> 이걸 implements ScheduleRepository한 클래스는 이 인터페이스의 모든 메서드를 꼭 구현해야 함!
-
-    Schedule saveSchedule(Schedule schedule); // 일정 생성, 수정
-
-    List<Schedule> findAllSchedules();  // 전체 일정 조회
-
-    Schedule findScheduleById(Long id);  // 단건 일정 조회
-
-    void deleteSchedule(Long id); // 일정 삭제
-
-    Schedule updateSchedule(Schedule schedule); // 일정 수정
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 }
+
